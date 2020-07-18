86d310bb2d4cbbb40784b7604ef1dfbcfe9a3bb3
Juan Felipe Calao Perez <jfcalao@uninorte.edu.co>
2020-07-17 19:22:14 -0500

    maquetando main view

---
+++ src/components/Contact.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ContactStyled= styled.div`
+  color: #FFFAFA;
+  i{
+    font-size: 66px;
+  }
+`
+
+const Contact =()=>{
+
+return(
+  <ContactStyled>
+    <i className="fab fa-whatsapp"></i>
+    <p>Contactanos por whatsapp</p>
+  </ContactStyled>
+)
+
+}
+export default Contact
