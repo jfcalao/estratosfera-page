adab1699d947ab429195195bccf19a14004cc69b
Juan Felipe Calao Perez <jfcalao@uninorte.edu.co>
2020-07-22 17:16:04 -0500

    fixing js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
 const menu = document.querySelector(".menu")
 const hamburgerButton = document.querySelectorAll('.burger-button')
-const container = document.querySelector('.container')
+const container = document.querySelector('.opacity-wraper')
 const menuItemList = document.querySelectorAll('.menu a')
-const handleClick = () => {
-  toggleMenu()
+const toggleOpacity = () => {
+  if (menu.classList.contains('is-active')) {
+    container.classList.add('opaco')
+  }else{
+    container.classList.remove('opaco')
+  }
 }
 const toggleMenu = () => {
   if (menu.classList.contains('is-active')) {
     menu.classList.remove('is-active')
     hamburgerButton[0].classList.add('hide-icon')
     hamburgerButton[1].classList.remove('hide-icon')
-    container.classList.remove('opaco')
   } else {
     menu.classList.add('is-active')
     hamburgerButton[1].classList.add('hide-icon')
     hamburgerButton[0].classList.remove('hide-icon')
-    container.classList.add('opaco')
   }
 }
 
 hamburgerButton.forEach(element => {
-  element.addEventListener('click', handleClick)
+  element.addEventListener('click', ()=>{toggleMenu()
+    toggleOpacity()
+  })
 });
 menuItemList.forEach(element => {
   element.addEventListener('click', toggleMenu)
