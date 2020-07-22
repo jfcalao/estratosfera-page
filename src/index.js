bee93dce0904847d9ba28c5b08bd00bbe427aad5
Juan Felipe Calao Perez <jfcalao@uninorte.edu.co>
2020-07-21 22:35:40 -0500

    toggle menu icon

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 const menu = document.querySelector(".menu")
-const hamburgerButton= document.querySelector('.burger-button')
-const container= document.querySelector('.container')
-const menuItemList= document.querySelectorAll('.menu a')
-const handleClick=()=>{
+const hamburgerButton = document.querySelectorAll('.burger-button')
+const container = document.querySelector('.container')
+const menuItemList = document.querySelectorAll('.menu a')
+const handleClick = () => {
   toggleMenu()
 }
-const toggleMenu= ()=>{
-  if(menu.classList.contains('is-active')){
+const toggleMenu = () => {
+  if (menu.classList.contains('is-active')) {
     menu.classList.remove('is-active')
+    hamburgerButton[0].classList.add('hide-icon')
+    hamburgerButton[1].classList.remove('hide-icon')
     container.classList.remove('opaco')
-  }else{
+  } else {
     menu.classList.add('is-active')
+    hamburgerButton[1].classList.add('hide-icon')
+    hamburgerButton[0].classList.remove('hide-icon')
     container.classList.add('opaco')
   }
 }
-hamburgerButton.addEventListener('click', handleClick)
+
+hamburgerButton.forEach(element => {
+  element.addEventListener('click', handleClick)
+});
 menuItemList.forEach(element => {
   element.addEventListener('click', toggleMenu)
 });
